d22027fbd65e4d016024d65b14b163fa900a0137
hodgint <hodgint@oregonstate.edu>
2019-11-10 14:58:18 -0800

    Added draw array.

--- js/sort.js
+++ js/sort.js
@@ -1,6 +1,54 @@
 var sort = (function(){
 
     /*
+    * Draws boxes to display given array with
+    * Colors given
+    * Inputs:
+    * - arr: an array of numbers
+    * - canvas: DOM object where to draw
+    * - colors: array of strings. ith 
+    * element is the color of the ith element in arr.
+    */
+   function drawArray(arr, canvas, colors){
+        /* canvas setup */
+        var context = canvas.context('2d');
+        var width = 2;
+        var size = arr.length;
+        var spacing = canvas.width / ( width * n + n + 1);
+        var barWidth = spacing * width; 
+
+        /* find min/max */
+        var min = arr[0];
+        var max = arr[0];
+        for(var i = 0; i < size; i++){
+            min = arr[i] < min ? arr[i] : min;
+            max = arr[i] > max ? arr[i] : max;
+        }
+
+        /* utility function  to bounds y to between 0 and canvas height */
+       function boundY(y){
+           var a = canvas.height / (min - max);
+           var b = canvas.height / (max - min);
+           return a * y + b;
+       }
+
+        /* canvas border */
+        context.strokeRect(0,0,canvas.width, canvas.height);
+        /* draw boxes */
+        var x = spacing;
+        context.beginPath()
+        context.moveTo(0,0);
+        context.lineTo(canvas.width, 0);
+        for(var i = 0; i < size; i++){
+            context.fillStyle = colors[i];
+            var y = boundY(arr[i]);
+            context.fillRect(x, y, barwidth, 0, )
+        }
+        x  = x + spacing + barWidth
+
+       }
+
+    /*
     * Returns the difference between
     * two elements in the given array
     * Inputs: 
@@ -26,10 +74,10 @@ var sort = (function(){
     }
     /* 
     * Sorts given array using bubble sort
-    * -arr: an array of numbers
+    * - arr: an array of numbers
     */
    function bubbleSort(arr) {
-        var size = arr.length();
+        var size = arr.length;
         for(var i = 0; i < size; i++){
             for(var j = 0; j < size - i - 1; j++){
                 if(compare(arr,j+1, j) < 0){
