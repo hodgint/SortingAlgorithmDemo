6770493cd68a463a69604bbf0ccc923c22acdb6f
hodgint <hodgint@oregonstate.edu>
2019-11-11 14:59:31 -0800

    added animate array utility functions.

--- js/sort.js
+++ js/sort.js
@@ -1,5 +1,20 @@
 var sort = (function(){
 
+    const DEFAULT_COLOR = '#777';
+    const SWAP_COLOR = '#FF0000';
+    const COMPARE_COLOR = '#008000'
+
+
+    /*
+    * Returns a random number between two given numbers
+    * Inputs:
+    * - high: highest the random number can be
+    * - low: lowest the random number can be
+    */
+    function randInt(high, low){
+        return low + Math.floor((high - low + 1) * Math.random);
+    }
+
     /*
     * Draws boxes to display given array with
     * Colors given
@@ -46,7 +61,64 @@ var sort = (function(){
         }
         x  = x + spacing + barWidth
 
-       }
+    }
+
+    /*
+    * Animates the boxes on the canvas
+    * Inputs:
+    * - arr: an array of numbers
+    * - canvas: DOM object where we draw
+    */
+    function animateArr(arr, canvas){
+        this._arr = arr;
+        this._canvas = canvas;
+        this._displayArr = [];
+        this._colors = [];
+        this._actions = [];
+        for(var i = 0; i < arr.length; i++){
+            this._displayArr.push(arr[i]);
+            this._colors.push(DEFAULT_COLOR);
+        }
+        drawArray(this._arr, this._canvas, this._colors);
+        animateArr.prototype._swap = function(i, j){
+            this._actions.push(['swap', i, j]);
+            var temp = arr[i];
+            arr[i] = arr[j]
+            arr[j] = temp;
+        }
+        animateArr.prototype._compare = function(i, j){
+            this._actions.push(['compare', i, j])
+            return arr[i] - arr[j];
+        }
+        animateArr.prototype.greaterThan = funciton(i, j){
+            return this.compare(i, j) > 0;
+        }
+        animateArr.prototype.lessThan = funciton(i, j){
+            return this.compare(i, j) < 0;
+        }
+        animateArr.prototype._step = function(){
+            if(this._actions.length === 0){
+                drawArray(this._displayArr, this._canvas, this._colors);
+                return;
+            }
+            var action = this._actions.shift();
+            var i = action[1];
+            var j = action[2];
+            if(action[0] === 'swap'){
+                this._colors[i] = SWAP_COLOR;
+                this._colors[j] = SWAP_COLOR;  
+            } else if (action[0] === 'compare'){
+                this._colors[i] = COMPARE_COLOR;
+                this._colors[j] = COMPARE_COLOR;
+                var temo = this.displayArr[i];
+                this._displayArr[i] = this.displayArr[j];
+                this.displayArr[j] = temp;
+            }
+            drawArray(this._displayArr, this._canvas, this._colors);
+            this._colors[i] = DEFAULT_COLOR;
+            this._colors[j] = DEFAULT_COLOR;
+        }
+    }
 
     /*
     * Returns the difference between
@@ -86,4 +158,12 @@ var sort = (function(){
             }
         }
     }
-})
+    var algorithms = {
+        'bubble': bubbleSort
+    }
+
+    return {
+        animatArr: animateArr,
+        algorithms: algorithms
+    }
+})();
